9afa8dfaa0e5f7449ea68af6f5c47a3eb7153a41
Tate Green <tategreen@Tates-MacBook-Pro.local>
2019-04-23 14:13:34 -0400

    map

--- census-app/app.R
+++ census-app/app.R
@@ -8,6 +8,17 @@
 #
 
 library(shiny)
+library(maps)
+library(mapproj)
+
+counties <- readRDS("census-app/counties.rds")
+
+source("census-app/helpers.R")
+
+percent_map(counties$white, "darkgreen", "% White")
+
+
+
 
 ui <- fluidPage(
   titlePanel("censusVis"),
@@ -30,16 +41,32 @@ ui <- fluidPage(
                   min = 0, max = 100, value = c(0, 100))
       ),
     
-    mainPanel(
-      textOutput("selected_var")
-    )
-  )
-  )
+mainPanel(plotOutput("map"))
+)
+)
+
 server <- function(input, output) {
+  output$map <- renderPlot({
+    data <- switch(input$var, 
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
   
-  output$selected_var <- renderText({ 
-    paste("You have selected this", input$var)
+      color <- switch(input$var, 
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
+    
+    legend <- switch(input$var, 
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
+    
+    percent_map(data, color, legend, input$range[1], input$range[2])
   })
-  
 }
+
 shinyApp(ui = ui, server = server)
